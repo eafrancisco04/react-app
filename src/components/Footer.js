d577ea9b3739d94d7debd7b309808a5582d8b61e
Ea Francisco <emberkasai04@gmail.com>
2022-01-27 17:45:43 -0500

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+        <footer>
+            <p>Copyrighted by Elizabeth Francisco. All rights reserved</p>
+        </footer>
+
+    )
+
+}
+
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header'
+import Footer from './components/Footer'
 import "./css/style.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <Header></Header>
+    <Footer></Footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
