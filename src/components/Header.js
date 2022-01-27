8b5cbef2aacdb7397fec7feb3f281598258e8363
Ea Francisco <emberkasai04@gmail.com>
2022-01-27 15:55:33 -0500

    organize files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+    /*html here */
+        <header>
+            <h1>Title</h1>
+        </header>
+
+    )
+
+}
+
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
