f9a21103614be50341f68f46b00c902090d48e40
Ea Francisco <emberkasai04@gmail.com>
2022-01-27 19:30:48 -0500

    Create Portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,15 @@
+
+
+function Portfolio(props) {
+    return(
+      <article>
+        {/* <h2>{props.heading}</h2>
+        <p>{props.paragraph}</p> */}
+
+        <h2>Blog Title</h2>
+        <p>Description</p>
+      </article>
+    )
+  }
+
+  export default Portfolio
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Bio from './components/Bio'
+import Portfolio './components/Portfolio'
 import "./css/style.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <Header></Header>
     <Bio></Bio>
+    <Portfolio></Portfolio>
+    <Portfolio></Portfolio>
+    <Portfolio></Portfolio>
     <Footer></Footer>
   </React.StrictMode>,
   document.getElementById('root')
