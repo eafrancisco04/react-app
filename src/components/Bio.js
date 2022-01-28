aae86938e0d636bba363ce498067ffbe8c20f8ef
Ea Francisco <emberkasai04@gmail.com>
2022-01-27 19:09:33 -0500

    Create Bio component

---
+++ src/components/Bio.js
@@ -0,0 +1,13 @@
+function Bio(){
+    return(
+    /*html here */
+        <section>
+            <p> Text here</p>
+        </section>
+
+    )
+
+}
+
+
+export default Bio
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Bio from './components/Bio'
 import "./css/style.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <Header></Header>
+    <Bio></Bio>
     <Footer></Footer>
   </React.StrictMode>,
   document.getElementById('root')
