d14dfab168208f2907f8c8c1d0eae67c53e100f7
Ea Francisco <emberkasai04@gmail.com>
2022-02-01 16:09:53 -0500

    Fix style so that it is view height

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,11 +1,14 @@
 function Bio(){
     return(
     /*html here */
-        <section class="bio">
-            <h2>My name is Elizabeth Ann Francisco</h2>
-            <h3> Welcome to my sample portfolio website.</h3>
-            <p>My name is Elizabeth Ann Francisco. I am a freelance writer and content manager from the Philippines. I am currently studying Web Design and Development at Humber College. I have a bachelor's degree in International Studies, major in European Studies from De La Salle University.
-            </p>
+        <section>
+            <div class ="bio">
+                <h2>My name is Elizabeth Ann Francisco</h2>
+                <h3> Welcome to my sample portfolio website.</h3>
+                <br></br>
+                <p class="intro">I am a freelance writer and content manager from the Philippines. I am currently studying Web Design and Development at Humber College. I have a bachelor's degree in International Studies, major in European Studies from De La Salle University.
+                </p>
+            </div>
         </section>
 
     )
